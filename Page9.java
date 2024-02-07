6a1faf5ef7635c0a39498733188c0b962ae051bc
bendbjij <sdmfkds@gmail.com>
2024-02-07 14:41:19 -0500

    sdsa
    sdsadsadsa

---
+++ Page9.java
@@ -0,0 +1,6 @@
+public class Page9 {
+    public static void (String[] args) {
+        System.out.println("Hello World RAAAH");
+        System.out.println("I know java up until inheritance raaah");
+    }
+}
